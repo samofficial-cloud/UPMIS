fbf5ca5c0caf5d5a164cb630c04489cd664d4a20
samson <samforbuilding@gmail.com>
2021-06-24 12:06:05 +0300

    restored debugbar provider class

--- config/app.php
+++ config/app.php
@@ -182,6 +182,7 @@ return [
        //dompdf
         Barryvdh\DomPDF\ServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
